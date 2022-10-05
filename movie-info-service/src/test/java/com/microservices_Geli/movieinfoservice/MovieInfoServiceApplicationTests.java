31d1e20a24b11fc90fde097f073804026d8a2098
Geli Yang <gyang5@villanova.edu>
2022-10-05 17:27:01 -0400

    initial commit for movie-info-service
    initial commit for movie-info-service

---
+++ movie-info-service/src/test/java/com/microservices_Geli/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices_Geli.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
