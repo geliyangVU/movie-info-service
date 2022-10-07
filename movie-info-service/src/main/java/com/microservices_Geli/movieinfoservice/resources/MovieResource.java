571bff3661c72c133aa074978af6ecf17039ab89
Geli Yang <gyang5@villanova.edu>
2022-10-07 11:49:51 -0400

    Implement Movie class
    Movie class has two member fields, movieId and name

---
+++ movie-info-service/src/main/java/com/microservices_Geli/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,10 @@
+package com.microservices_Geli.movieinfoservice.resources;
+
+import com.microservices_Geli.movieinfoservice.models.Movie;
+
+public class MovieResource {
+
+    public Movie getMovieInfo(){
+        return null;
+    }
+}
