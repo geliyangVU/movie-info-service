a70d9548345caeb468971e09365aaeb56223fcfb
Geli Yang <gyang5@villanova.edu>
2022-10-07 11:56:43 -0400

    Implemented getMovieInfo endpoint
    Tested using postman. Configured the server port to 8084 in order to run locally.

--- movie-info-service/src/main/java/com/microservices_Geli/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/src/main/java/com/microservices_Geli/movieinfoservice/resources/MovieResource.java
@@ -1,10 +1,17 @@
 package com.microservices_Geli.movieinfoservice.resources;
 
 import com.microservices_Geli.movieinfoservice.models.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
+@RequestMapping("/movies")
 public class MovieResource {
 
-    public Movie getMovieInfo(){
-        return null;
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId){
+        return new Movie(movieId, "test Movie");
     }
 }
